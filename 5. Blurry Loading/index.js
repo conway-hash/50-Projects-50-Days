94363a6a37b69340bcef72be18ee8b8cf259ef3c
Tomáš Bokor <tomas.bokor10@gmail.com>
2022-07-22 14:51:38 +0200

    Day5

---
+++ 5. Blurry Loading/index.js
@@ -0,0 +1,16 @@
+const bg = document.getElementById('blur')
+const n = document.getElementById('countdown')
+
+
+for (let i = 0; i < 101; i++) {
+    loop(i);
+};
+
+
+function loop(i) {
+    setTimeout(function () {
+        n.innerText = i + '%';
+        n.style.opacity = (100 - i) / 100;
+        bg.style.filter = `blur(${100 - i}px)`;
+    }, 30 * i);
+}
