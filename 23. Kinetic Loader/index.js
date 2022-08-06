8714667c3eee8fd4fe6d198a0ab0f2bcd5697b48
Tomáš Bokor <tomas.bokor10@gmail.com>
2022-08-06 23:56:48 +0200

    Day23v1

---
+++ 23. Kinetic Loader/index.js
@@ -0,0 +1,7 @@
+const first = document.querySelector('.first')
+const second = document.querySelector('.second')
+
+setTimeout(1000)
+first.style.transform = 'rotate(180deg)';
+setTimeout(1000)
+second.style.transform = 'rotate(180deg)';
